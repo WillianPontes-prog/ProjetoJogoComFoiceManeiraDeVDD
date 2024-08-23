f5bc3c23b9bd11d5c9790d37ffc867261f308bb5
Myster Koala <mysterkoala@gmail.com>
2024-08-23 17:38:34 -0300

    oioioi

---
+++ ProjetoJogoComFoiceManeira/Icones.cpp
@@ -0,0 +1,18 @@
+#include "Icones.h"
+
+Icones::Icones(sf::Vector2f posicao, int quantidade) :
+	posicao (posicao),
+	quantidade(quantidade)
+{
+	
+}
+
+Icones::~Icones()
+{
+}
+
+
+
+void Icones::draw()
+{
+}
